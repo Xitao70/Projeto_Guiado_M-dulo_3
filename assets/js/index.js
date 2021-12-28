f0ac798418aeaab74c477c117723b1317926be51
WASHINGTON <87780829+Xitao70@users.noreply.github.com>
2021-12-27 22:02:24 -0300

    Add files via upload
    upload dos arquivos form e index.js

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+    
+        var pessoasRaw = localStorage.getItem('pessoas')
+        if(pessoasRaw != null){
+            var pessoas = JSON.parse(pessoasRaw)
+        }else{
+            var pessoas = [];
+        }
+    function desenhaTabela(){
+ 
+        
+        console.log(document.querySelectorAll('table.lista tbody .conteudo_dinamico'))
+
+        linhaAtuais = [...document.querySelectorAll('table.lista tbody .conteudo_dinamico')]
+        linhaAtuais.forEach((element) => {
+            element.remove();
+        });
+        for (person in pessoas){
+                //console.log(person)
+                document.querySelector('table.lista tbody').innerHTML += 
+                `<tr class="conteudo_dinamico" style ='background-color: ${((person%2==0) ? '#FFF' : '#eee')}'>
+                <td>
+                    ${pessoas[person].name}
+                </td>
+                <td>
+                    ${pessoas[person].tel}
+                </td>
+                <td>
+                    ${(pessoas[person].exp ? '<strong style="color:blue">Sim</strong>' : '<strong style="color:red">Não</strong>') }
+                </td>
+                <td>
+                    <button onclick="apagaUsuario(${person})">Excluir</button>
+                    <a href="./src/form.html?person=${person}">Editar</a>
+                </td>
+            </tr>
+                `
+        }
+}
+function apagaUsuario(p) {
+    pessoas.splice(p, 1); 
+    desenhaTabela();
+     localStorage.setItem('pessoas', JSON.stringify(pessoas))
+} 
+desenhaTabela()
